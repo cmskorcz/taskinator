f7e51625365fe303727d5c37a16c8fff18b29ea3
Chase Skorczewski <chaseskorey@icloud.com>
2022-04-13 14:18:01 -0400

    form takes user data and appends to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,18 @@ const tasksToDoEl = document.querySelector("#tasks-to-do");
 
 const creatTaskHandler = (event) => {
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
     let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item"
-    listItemEl.textContent = "This is a new task";
+    listItemEl.className = "task-item";
+
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = `<h3 class="task-name">${taskNameInput}</h3><span class="task-type">${taskTypeInput}</span>`;
+
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 }
 
