38fb5c75e18be5391643d1e77dff8fb20e2bf60b
Chase Skorczewski <chaseskorey@icloud.com>
2022-04-13 14:42:59 -0400

    added form validation and form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ const taskFormHandler = (event) => {
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     let taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
